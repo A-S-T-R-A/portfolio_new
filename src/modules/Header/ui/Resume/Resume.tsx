3a0b4ddbb024dca1f92fb141e4c0b05af478993c
Art1us <109896229+Art1us@users.noreply.github.com>
2023-05-12 12:44:35 +0300

    test

--- src/modules/Header/ui/Resume/Resume.tsx
+++ src/modules/Header/ui/Resume/Resume.tsx
@@ -1,19 +1,22 @@
 import { useState } from "react"
+import { Document, Page } from "react-pdf/dist/esm/entry.vite"
 import { Button, ButtonColor } from "shared/ui/Button/Button"
 import styles from "./Resume.module.scss"
 import pdf from "shared/assets/resume/cv.pdf"
 
 export function Resume() {
-    const [isVisible, setIsVisible] = useState(false)
+    // const [isVisible, setIsVisible] = useState(false)
+
+    function clickHandler() {
+        window.open("/cv.pdf", "_blank")
+    }
 
     return (
         <>
-            {isVisible && <iframe src={pdf} className={styles.pdf}></iframe>}
-            <Button
-                color={ButtonColor.INVERTED}
-                className={styles.btn}
-                onClick={() => setIsVisible(true)}
-            >
+            <a href="/cv.pdf" target="_blank" rel="noopener noreferrer">
+                Open PDF
+            </a>
+            <Button color={ButtonColor.INVERTED} className={styles.btn} onClick={clickHandler}>
                 Resume
             </Button>
         </>
