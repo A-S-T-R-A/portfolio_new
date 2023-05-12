fafc44af35f0d5fdbe394427f4c68bd382c88591
Art1us <109896229+Art1us@users.noreply.github.com>
2023-05-12 12:45:54 +0300

    fixes

--- src/modules/Header/ui/Resume/Resume.tsx
+++ src/modules/Header/ui/Resume/Resume.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react"
-import { Document, Page } from "react-pdf/dist/esm/entry.vite"
 import { Button, ButtonColor } from "shared/ui/Button/Button"
 import styles from "./Resume.module.scss"
-import pdf from "shared/assets/resume/cv.pdf"
 
 export function Resume() {
-    // const [isVisible, setIsVisible] = useState(false)
-
     function clickHandler() {
         window.open("/cv.pdf", "_blank")
     }
