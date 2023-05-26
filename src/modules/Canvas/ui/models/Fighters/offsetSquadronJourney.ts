672cc199c5b91ea108426e97afe3d7af26a1c733
Art1us <109896229+Art1us@users.noreply.github.com>
2023-05-26 15:57:53 +0300

    added fighters turning

--- src/modules/Canvas/ui/models/Fighters/offsetSquadronJourney.ts
+++ src/modules/Canvas/ui/models/Fighters/offsetSquadronJourney.ts
@@ -1,8 +1,8 @@
-import { Journey, Position } from "../../../types/types"
+import { Journey, Position, Speed } from "../../../types/types"
 
 export function offsetSquadronJourney(journey: Journey, index: number): Journey {
     if (index === 0) return journey
-    const SPREAD_X = 2
+    const SPREAD_X = 1.5
     const SPREAD_Y = 0.25
     const SPREAD_Z = 3.75
 
@@ -10,21 +10,27 @@ export function offsetSquadronJourney(journey: Journey, index: number): Journey 
         const [x, y, z] = psn
 
         const isOdd = index && index % 2 !== 0
-        const newX = isOdd ? x - SPREAD_X * (index + 1) : x + SPREAD_X * index
-        const newY = isOdd ? y + SPREAD_Y * (index + 1) : y + SPREAD_Y * index
-        const newZ = isOdd ? z - SPREAD_Z * (index + 1) : z - SPREAD_Z * index
+
+        let newX = x + SPREAD_X * index
+        let newY = y + SPREAD_Y * index
+        let newZ = z - SPREAD_Z * index
+
+        if (isOdd) {
+            newX = x - SPREAD_X * (index + 1)
+            newY = y + SPREAD_Y * (index + 1)
+            newZ = z - SPREAD_Z * (index + 1)
+        }
 
         return [newX, newY, newZ]
     }
 
-    const arr = journey.map(item => {
+    const arr = journey.map((item, wpIndex) => {
         const newItem = [...item]
 
-        // @ts-ignore
-        newItem[0] = offsetCoordinates(newItem[0])
-        return newItem
+        const [x, y, z] = offsetCoordinates(newItem[0] as Position)
+        newItem[0] = [x, y, wpIndex > 0 ? z + 10 + index : z]
+        return newItem as [Position] | [Position, Speed]
     })
 
-    // @ts-ignore
     return arr
 }
