a2fbc51c7c4c7dad16c9fb2f42c801f4e6b9eed6
VadimGalian <vadim.galian@gmail.com>
2023-05-27 15:40:54 +0300

    Update index.ts

--- src/shared/assets/images/background/index.ts
+++ src/shared/assets/images/background/index.ts
@@ -3,6 +3,6 @@ import planetMobile from "./planetMobile.webp"
 import planetTablet from "./planetTablet.webp"
 import planetLaptop from "./planetLaptop.webp"
 import planetDesktopFull from "./planetDesktopFull.webp"
-import planetDesktop4K from "./planetDesktop4K.webp"
+import planetDesktop4K from "./planetDesktop4k.webp"
 
 export { planetMobile, planetTablet, planetLaptop, planetDesktopFull, planetDesktop4K, stars }
