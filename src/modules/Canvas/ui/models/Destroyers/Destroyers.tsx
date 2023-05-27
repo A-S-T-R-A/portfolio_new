a02f1546a8cc9a6e21e26507232ec88eec2106d5
Art1us <109896229+Art1us@users.noreply.github.com>
2023-05-27 13:32:25 +0300

    fix netlify

--- src/modules/Canvas/ui/models/Destroyers/Destroyers.tsx
+++ src/modules/Canvas/ui/models/Destroyers/Destroyers.tsx
@@ -5,7 +5,7 @@ import { Destroyer } from "./Destroyer/Destroyer"
 export function Destroyers(): ReactElement {
     return (
         <>
-            {destroyers.map((item, index) => {
+            {destroyers.map(item => {
                 const { id } = item
                 return <Destroyer key={id} data={item} />
             })}
