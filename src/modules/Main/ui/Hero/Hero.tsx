abe5efaed77e1417424cd1c6435338b78678f222
Artur <109896229+Art1us@users.noreply.github.com>
2023-10-22 17:44:45 +0300

    Update Hero.tsx

--- src/modules/Main/ui/Hero/Hero.tsx
+++ src/modules/Main/ui/Hero/Hero.tsx
@@ -15,7 +15,7 @@ function Hero() {
                 I'm a <span className={styles.accent}>Front End Developer</span>
             </h1>
             <p className={styles.text}>
-                Hi, my name is Artur. I am software engineer with more than 5 years of experience
+                Hi, my name is Artur. I am software engineer with more than 6 years of experience
                 with such technologies: React, Redux, TS. I worked in such business domains as:
                 E-commerce, Real Estate and Communication/Messaging.
             </p>
