85b84d7e317f05c7fb05cfc1e26ef08fb2e917c5
Artur P <artur0503915024@gmail.com>
2024-04-06 16:34:24 +0300

    upd 6-apr-24

--- src/modules/Main/ui/Hero/Hero.tsx
+++ src/modules/Main/ui/Hero/Hero.tsx
@@ -1,30 +1,29 @@
-import { BsArrowRightCircle } from "react-icons/bs"
-import { Button, ButtonVariant } from "shared/ui/Button/Button"
-import styles from "./Hero.module.scss"
+import { BsArrowRightCircle } from "react-icons/bs";
+import { Button, ButtonVariant } from "shared/ui/Button/Button";
+import styles from "./Hero.module.scss";
 
 function Hero() {
-    function clickHandler() {
-        window.open("/cv.pdf", "_blank")
-    }
+  function clickHandler() {
+    window.open("/cv.pdf", "_blank");
+  }
 
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.greeting}>
-                Hi! My name is Artur
-                <br />
-                I'm a <span className={styles.accent}>Front End Developer</span>
-            </h1>
-            <p className={styles.text}>
-                Hi, my name is Artur. I am software engineer with more than 6 years of experience
-                with such technologies: React, Redux, TS. I worked in such business domains as:
-                E-commerce, Real Estate and Communication/Messaging.
-            </p>
-            <Button onClick={clickHandler} variant={ButtonVariant.CLEAR} className={styles.connect}>
-                <p>My Resume</p>
-                <BsArrowRightCircle className={styles.icon} />
-            </Button>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.greeting}>
+        Hi! My name is Artur
+        <br />
+        I'm a <span className={styles.accent}>Front End Developer</span>
+      </h1>
+      <p className={styles.text}>
+        A passionate frontend developer with 7+ years of experience in React, Next.js and Typescript. Committed to
+        continuous learning and staying up-to-date with the latest technologies.
+      </p>
+      <Button onClick={clickHandler} variant={ButtonVariant.CLEAR} className={styles.connect}>
+        <p>My Resume</p>
+        <BsArrowRightCircle className={styles.icon} />
+      </Button>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
--- src/shared/components/SocialIcons/data.tsx
+++ src/shared/components/SocialIcons/data.tsx
@@ -1,16 +1,16 @@
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai"
-import { FaTelegramPlane } from "react-icons/fa"
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { FaTelegramPlane } from "react-icons/fa";
 
 export const socialsData = [
-    {
-        id: 1,
-        link: "https://t.me/artius",
-        icon: <FaTelegramPlane />,
-    },
-    {
-        id: 2,
-        link: "https://www.linkedin.com/in/artur-pazyniuk/",
-        icon: <AiFillLinkedin />,
-    },
-    { id: 3, link: "https://github.com/Art1us", icon: <AiFillGithub /> },
-]
+  {
+    id: 1,
+    link: "https://t.me/artius",
+    icon: <FaTelegramPlane />
+  },
+  {
+    id: 2,
+    link: "https://www.linkedin.com/in/artur-paz/",
+    icon: <AiFillLinkedin />
+  },
+  { id: 3, link: "https://github.com/Art1us", icon: <AiFillGithub /> }
+];
